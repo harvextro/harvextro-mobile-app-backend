df1fdabf4022b57cfca419ae3e5fdc120a369f5f
Manthisi Herath <manthisi.20240838@iit.ac.lk>
2026-03-15 23:04:16 +0530

    loggin with google

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,5 +1,10 @@
 import { auth } from "../firebase";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { GoogleAuthProvider } from "@react-native-firebase/auth";
 
+/**
+ * Email / Password Login
+ */
 export const loginWithEmail = async (email, password) => {
   try {
 
@@ -17,6 +22,35 @@ export const loginWithEmail = async (email, password) => {
   } catch (error) {
 
     console.error("Login failed:", error.message);
+
+    throw error;
+
+  }
+};
+
+/**
+ * Google Login
+ */
+export const loginWithGoogle = async () => {
+  try {
+
+    await GoogleSignin.hasPlayServices();
+
+    const userInfo = await GoogleSignin.signIn();
+
+    const googleCredential = GoogleAuthProvider.credential(
+    userInfo.idToken
+    );
+
+    const userCredential = await auth().signInWithCredential(googleCredential);
+
+    console.log("Google login successful:", userCredential.user.email);
+
+    return userCredential.user;
+
+  } catch (error) {
+
+    console.error("Google login error:", error);
 
     throw error;
 
