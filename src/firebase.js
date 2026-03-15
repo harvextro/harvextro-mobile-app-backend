bd5cdc29af44ca60eb4eee3eb2d655c308fdb4cc
Manthisi Herath <manthisi.20240838@iit.ac.lk>
2026-03-15 21:04:44 +0530

    add firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,4 @@
+import auth from "@react-native-firebase/auth";  // Import Firebase Authentication module
+import firestore from "@react-native-firebase/firestore";  // Import Firebase Firestore database module
+
+export { auth, firestore };  // Export auth and firestore so they can be used in other files
