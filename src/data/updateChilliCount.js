737827405c17f473db5e27ec6e8cf0c3496f605d
Manthisi Herath <manthisi.20240838@iit.ac.lk>
2026-03-16 01:26:48 +0530

    to update chilli count

--- src/data/updateChilliCount.js
+++ src/data/updateChilliCount.js
@@ -1,1 +1,25 @@
 import firestore from "@react-native-firebase/firestore";
+
+
+export const updateChilliCount = async (type) => {
+
+  const ref = firestore().collection("summary").doc("counts");
+
+  await firestore().runTransaction(async (transaction) => {
+
+    const doc = await transaction.get(ref);
+
+    const data = doc.data();
+
+    transaction.update(ref, {
+
+      [type]: (data[type]) + 1,
+      total: (data.total) + 1
+
+    });
+
+  });
+
+  console.log("Updated chilli:", type);
+  
+};
