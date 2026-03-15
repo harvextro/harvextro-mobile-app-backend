f15a9919bb53268c00c0d0699a8a83e2861f174d
Manthisi Herath <manthisi.20240838@iit.ac.lk>
2026-03-16 01:28:40 +0530

    getting the summery

--- src/data/getSummary.js
+++ src/data/getSummary.js
@@ -1,1 +1,17 @@
 import firestore from "@react-native-firebase/firestore";
+
+export const getSummary = async () => {
+
+  const doc = await firestore()
+
+    .collection("summary")
+    .doc("counts")
+    .get();
+
+  if (doc.exists) {
+    return doc.data();
+  }
+
+  return null;
+  
+};
