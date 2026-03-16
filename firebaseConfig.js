e8ad450dbb9ffd4391e7872b17afa3c3a1737550
Thinara Senarathne <thinarasenarathne@gmail.com>
2026-03-15 18:22:19 -0800

    firebase config created

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXXX",
+  appId: "XXXXX",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
